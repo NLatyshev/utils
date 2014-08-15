b49953d9f9d5af1593d8273a10e882c1041d0927
Nikolay Latyshev <nikolay.latyshev@db.com>
2014-08-15 18:51:32 +0400

    test class

---
+++ proj/src/main/java/Test.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2008-2014
+ * LUXOFT
+ * All rights reserved.
+ *
+ * This product and related documentation are protected by copyright and
+ * distributed under licenses restricting its use, copying, distribution, and
+ * decompilation. No part of this product or related documentation may be
+ * reproduced in any form by any means without prior written authorization of
+ * LUXOFT and its licensors, if any.
+ *
+ * $Id$
+ */
+
+/**
+ * todo Document type Test
+ *
+ * @author NLatyshev
+ */
+public class Test {
+    private int count;
+
+}
